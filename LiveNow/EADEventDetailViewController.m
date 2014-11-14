6cffac4709eadf5dc083cf968b902f95498c53fd
Pravin Khabile <khabilepravin@gmail.com>
2014-11-13 21:03:36 -0500

    Segues to Messages and Users from Event
    Segue to messages and users from the event details page, also changed
    the event details to be of push segue instead of modal.

--- LiveNow/EADEventDetailViewController.m
+++ LiveNow/EADEventDetailViewController.m
@@ -9,6 +9,8 @@
 #import "EADEventDetailViewController.h"
 #import "Postman.h"
 #import "UserInfo.h"
+#import "EADMessageDetailsViewController.h"
+
 @interface EADEventDetailViewController ()
 
 @end
@@ -98,15 +100,22 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     if([segue.identifier isEqualToString:@"eventMessage"])
+     {
+         EADMessageDetailsViewController *destinationVC = [segue destinationViewController];
+         destinationVC.senderName = self.eventNameLabel.text;
+         destinationVC.eventId = eventId;
+         destinationVC.messangerType = @"Event";
+     
+     }
+ }
+
 
 @end
